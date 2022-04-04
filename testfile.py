c513970844f53bee066794e089f9e61d5941074c
soominji <0603ji@naver.com>
2022-04-04 19:47:23 +0900

    first commit

---
+++ testfile.py
@@ -0,0 +1,17 @@
+# 기본 계산기
+def add(a, b):
+  return a+b
+
+def subtract(a, b):
+  return a-b
+<<<<<<< HEAD
+
+def multiply(a, b):  
+    return a*b
+    
+def divide(a, b):  
+    return a/b
+=======
+def multiply(a,b);
+  return a*b
+>>>>>>> cbb7fff71311f48a6d35d5ac97c04791c6119b46
